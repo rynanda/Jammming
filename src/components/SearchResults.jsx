11b74edd1f53ccad23a0d037ef111cb6f3cc5239
rynanda <ryan.indrananda@gmail.com>
2025-06-05 20:06:56 +1000

    Add state and effect hooks from TrackList.jsx

--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {mockSpotifyData} from "./mockSpotifyData.js";
 
 import TrackList from "./TrackList.jsx";
 
 function SearchResults(props) {
+    const [tracks, setTracks] = useState([]);
+
+    useEffect(() => {
+        setTracks(mockSpotifyData.filter(track => track.trackName.toLowerCase().includes(props.search)));
+    }, [props.search]);
+
     return (
         <>
             <h3>Results: </h3>
-            {props.search && <TrackList search={props.search}/>}
+            <TrackList tracks={tracks} />
         </>
     )
 }

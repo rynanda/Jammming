ca53b97103b0b25bd4e2d2edeef0a5b68f1716ae
rynanda <ryan.indrananda@gmail.com>
2025-06-05 21:08:03 +1000

    Implement onRemove button onClick event handler for playlists

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useState} from 'react';
 import SearchResults from "./components/SearchResults.jsx";
 import SearchBar from "./components/SearchBar.jsx";
 import Playlist from "./components/Playlist.jsx";
@@ -8,21 +8,8 @@ import Playlist from "./components/Playlist.jsx";
 function App() {
     const [search, setSearch] = useState('');
     const [hasSearched, setHasSearched] = useState(false);
-    const [playlistName, setPlaylistName] = useState("");
+    const [playlistName, setPlaylistName] = useState("Playlist");
     const [playlistTracks, setPlaylistTracks] = useState([]);
-
-    const testPlaylist = {
-        name: "test playlist", tracks: [{
-            id: 3, name: "test track 1", artist: "test artist 1", album: "test album 1", added: true
-        }, {
-            id: 4, name: "test track 2", artist: "test artist 2", album: "test album 2", added: true
-        }]
-    }
-
-    useEffect(() => {
-        setPlaylistName(testPlaylist.name);
-        setPlaylistTracks(testPlaylist.tracks);
-    }, []);
 
     function onAdd(track) {
         if (playlistTracks.filter((existing) => existing.id === track.id).length === 0) {
@@ -32,10 +19,14 @@ function App() {
         }
     }
 
+    function onRemove(track) {
+        setPlaylistTracks((tracks) => tracks.filter((existing) => existing.id !== track.id));
+    }
+
     return (<>
         <SearchBar search={search} setSearch={setSearch} setHasSearched={setHasSearched}/>
         {hasSearched ? (<SearchResults search={search} onAdd={onAdd}/>) : (<SearchResults/>)}
-        <Playlist name={playlistName} tracks={playlistTracks}/>
+        <Playlist name={playlistName} tracks={playlistTracks} onRemove={onRemove}/>
     </>)
 }
 

625a4e652f3d74b0c365b933dba38b928d0d12b3
rynanda <ryan.indrananda@gmail.com>
2025-06-05 20:06:23 +1000

    Implement playlist component

---
+++ src/components/Playlist.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import TrackList from "./TrackList.jsx";
+
+function Playlist(props) {
+    return (
+        <>
+            <h3>{props.name}</h3>
+            <TrackList tracks={props.tracks} />
+        </>
+    )
+}
+
+export default Playlist;

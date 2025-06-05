c171ce37664757f168090cc67ffcb62b577e5846
rynanda <ryan.indrananda@gmail.com>
2025-06-05 15:32:22 +1000

    Create mock Spotify data

---
+++ src/components/mockSpotifyData.js
@@ -0,0 +1,5 @@
+export const mockSpotifyData = [{
+    id: 1, trackName: 'Wild is the Wind', artist: 'David Bowie', album: 'Station to Station'
+}, {
+    id: 2, trackName: 'Closer', artist: 'Nine Inch Nails', album: 'The Downward Spiral'
+}]

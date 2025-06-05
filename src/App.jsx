f4ae7ebc15d8cf3de39176b6f9784cca34c59422
rynanda <ryan.indrananda@gmail.com>
2025-06-05 20:07:17 +1000

    Test new playlist component implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,34 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import SearchResults from "./components/SearchResults.jsx";
 import SearchBar from "./components/SearchBar.jsx";
+import Playlist from "./components/Playlist.jsx";
 
 // import './App.css'
 
 function App() {
     const [search, setSearch] = useState('');
     const [hasSearched, setHasSearched] = useState(false);
-    return (
-        <>
+    const [playlistName, setPlaylistName] = useState("");
+    const [playlistTracks, setPlaylistTracks] = useState([]);
+
+    const testPlaylist = {
+        name: "test playlist", tracks: [{
+            id: 1, trackName: "test track 1", artist: "test artist 1", album: "test album 1", added: true
+        }, {
+            id: 2, trackName: "test track 2", artist: "test artist 2", album: "test album 2", added: true
+        }]
+    }
+
+    useEffect(() => {
+        setPlaylistName(testPlaylist.name);
+        setPlaylistTracks(testPlaylist.tracks);
+    }, []);
+
+    return (<>
             <SearchBar search={search} setSearch={setSearch} setHasSearched={setHasSearched}/>
             {hasSearched ? (<SearchResults search={search}/>) : (<SearchResults/>)}
-        </>
-    )
+            <Playlist name={playlistName} tracks={playlistTracks} />
+        </>)
 }
 
 export default App
